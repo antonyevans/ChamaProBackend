bca74991009406c5519ea0daf3e9d49757718cfe
Antony Evans <antonyevans@gmail.com>
2011-06-12 13:37:27 +0100

    rails initialised'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChamaProBackend::Application.config.secret_token = '65435aabce6b868b602c4ca13cfb634f507549bc93e645a9c5753fef46b4634e2ec9b8edef9c7dd5f2a7c231de6857dddfa0a211d9c33b53e72a688960dc52b8'
