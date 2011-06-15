4e6c9965fa2fc61c2613ab12865db951fa10f691
Antony Evans <antonyevans@gmail.com>
2011-06-15 12:28:37 +0100

    Added comment in rakefile to explain rake/dsl_definition

--- Rakefile
+++ Rakefile
@@ -2,7 +2,10 @@
 # for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
 
 require File.expand_path('../config/application', __FILE__)
+
+#This next line is required to run rake on heroku, but will crash when run locally so needs to be commented out
 require 'rake/dsl_definition'
+
 require 'rake'
 
 ChamaProBackend::Application.load_tasks
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,56 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110612141209) do
+ActiveRecord::Schema.define(:version => 20110615112116) do
+
+  create_table "balances", :force => true do |t|
+    t.string   "cliend_ID"
+    t.float    "balance"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "clients", :force => true do |t|
+    t.string   "client_ID"
+    t.string   "group_ID"
+    t.string   "firstName"
+    t.string   "surname"
+    t.string   "mobileNumber"
+    t.string   "email"
+    t.string   "gender"
+    t.string   "birthyear"
+    t.string   "photo"
+    t.datetime "created"
+    t.string   "type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "groups", :force => true do |t|
+    t.string   "group_ID"
+    t.string   "group_Name"
+    t.text     "description"
+    t.datetime "created"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "managerscashes", :force => true do |t|
+    t.string   "user_id"
+    t.float    "balance"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "transactions", :force => true do |t|
+    t.string   "transaction_ID"
+    t.string   "client_ID"
+    t.string   "type"
+    t.datetime "created"
+    t.float    "amount"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.text     "user_id"
